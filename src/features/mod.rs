7b670fa6f46378c080d911bce2d106e089ff6bd0
implicitlycorrect <121817193+implicitlycorrect@users.noreply.github.com>
2024-06-09 23:03:12 +0200

    add functions get_name and get_is_enabled for Feature trait

--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,6 +1,6 @@
 pub use cheatlib::*;
 
-use crate::context::Context;
+use crate::{config::Config, context::Context};
 
 pub mod bunnyhop;
 pub mod fov_changer;
@@ -8,5 +8,7 @@ pub mod no_flash;
 pub mod triggerbot;
 
 pub trait Feature {
+    fn get_name(&self) -> &str;
+    fn get_is_enabled(&self, config: &Config) -> bool;
     fn run(&mut self, context: &Context) -> Result<()>;
 }
